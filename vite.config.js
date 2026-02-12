13de40a6949360cc27111d98321623c39f719d01
Monique Fick <monique@seatrench.com>
2026-02-12 19:50:08 +0800

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/moniquefick-resume/',
   plugins: [react(), tailwindcss()],
 })
